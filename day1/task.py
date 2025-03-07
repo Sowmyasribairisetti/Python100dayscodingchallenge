c750245fb084baaddfa0a75cb395480d99fb7bb6
Sowmyasribairisetti <sowmyasribairisetti72@gmail.com>
2025-03-07 15:22:19 +0530

    Add files via upload

---
+++ day1/task.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator project")
+city=input("What's the name of the city you grew up in?")
+pet=input("What is your pet name?")
+print("Your band name could be: "+city+" "+ pet)
