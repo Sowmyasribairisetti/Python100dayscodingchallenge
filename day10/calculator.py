df6fd5444ef6e77796d47c82e6e668bb426f3eb5
Sowmyasribairisetti <sowmyasribairisetti72@gmail.com>
2025-03-16 14:22:48 +0530

    Create calculator.py

---
+++ day10/calculator.py
@@ -0,0 +1,90 @@
+'''The goal is to build a calculator program.
+
+Demo
+https://appbrewery.github.io/python-day10-demo/
+
+Storing Functions as a Variable Value
+You can store a reference to a function as a value to a variable. e.g.
+
+def add(n1, n2):
+    return n1 + n2
+    
+    
+my_favourite_calculation = add
+my_favourite_calculation(3, 5)  # Will return 8
+In the starting file, you'll see a dictionary that references each of the mathematical calculations that can be performed by our calculator. Try it out and see if you can get it to perform addition, subtraction, multiplication and division.
+
+PAUSE 1
+TODO: Write out the other 3 functions - subtract, multiply and divide.
+
+PAUSE 2
+TODO: Add these 4 functions into a dictionary as the values. Keys = "+", "-", "*", "/"
+
+PAUSE 3
+TODO: Use the dictionary operations to perform the calculations. Multiply 4 * 8 using the dictionary.
+
+Functionality
+Program asks the user to type the first number.
+Program asks the user to type a mathematical operator (a choice of "+", "-", "*" or "/")
+Program asks the user to type the second number.
+Program works out the result based on the chosen mathematical operator.
+Program asks if the user wants to continue working with the previous result.
+If yes, program loops to use the previous result as the first number and then repeats the calculation process.
+If no, program asks the user for the fist number again and wipes all memory of previous calculations.
+Add the logo from art.py
+ Hint 1 
+ Hint 2 '''
+
+import art
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# print(operations["*"](4, 8))
+
+
+def calculator():
+    print(art.logo)
+    should_accumulate = True
+    num1 = float(input("What is the first number?: "))
+
+    while should_accumulate:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
+
+
+calculator()
